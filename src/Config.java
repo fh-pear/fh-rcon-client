30cc73ac2e33937355478cea08c075cfc8135acd
fh-pear <pear.cod4@gmail.com>
2017-05-02 19:16:51 -0400

    implemented better settings for logging handle some of the logging configuration in class Config

--- src/Config.java
+++ src/Config.java
@@ -2,6 +2,7 @@ import javax.naming.ConfigurationException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Level;
 
 
 public final class Config
@@ -13,20 +14,39 @@ public final class Config
 	private static int serverPort;
 	private static String serverHost;
 	private static String mapImages;
-	private static int logLevel;
+	private static String logFormat;
+	private static Level logLevel;
 
 	public static void init(String fileName) throws NumberFormatException, IOException, ConfigurationException
 	{
 		Properties props = new Properties();
-		//InputStream is = new FileInputStream(fileName);
 		InputStream is = Config.class.getResourceAsStream(fileName);
 
 		props.load(is);
 
 		serverHost = props.getProperty("hostname");
 		mapImages = props.getProperty("map_images");
-		logLevel = Integer.parseInt(props.getProperty("log_level"));
+		logFormat = props.getProperty("log_format");
+		String level = props.getProperty("log_level").toUpperCase();
 
+		if (level.equals("ALL"))
+			logLevel = Level.ALL;
+		else if (level.equals("FINEST"))
+			logLevel = Level.FINEST;
+		else if (level.equals("FINER"))
+			logLevel = Level.FINER;
+		else if (level.equals("FINE"))
+			logLevel = Level.FINE;
+		else if (level.equals("CONFIG"))
+			logLevel = Level.CONFIG;
+		else if (level.equals("INFO"))
+			logLevel = Level.INFO;
+		else if (level.equals("WARNING"))
+			logLevel = Level.WARNING;
+		else if (level.equals("SEVERE"))
+			logLevel = Level.SEVERE;
+		else
+			logLevel = Level.OFF;
 
 		checkValues();
 	}
@@ -60,5 +80,9 @@ public final class Config
 	{
 		return mapImages;
 	}
+
+	public static Level getLoggingLevel() { return logLevel; }
+
+	public static String getLoggingFormat() { return logFormat; }
 
 }
