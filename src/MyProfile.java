413664945b70b9d4c1d15c043471e8a94c07d128
fh-pear <pear.cod4@gmail.com>
2017-05-02 00:51:58 -0400

    added Profile class for storing user info MyProfile class for displaying Profile data structure

---
+++ src/MyProfile.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MyProfile extends JFrame
+{
+	private JFrame frame;
+
+	public MyProfile()
+	{
+		frame = new JFrame("My Profile");
+	}
+}
---
+++ src/Profile.java
@@ -0,0 +1,10 @@
+/**
+ * Datastructure for holding user info. Username, admin level etc.
+ */
+
+import java.util.logging.Logger;
+
+public class Profile
+{
+	private static Logger logger = Logger.getLogger(LoginUI.class.getName());
+}
