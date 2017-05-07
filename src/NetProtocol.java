845bf82e0857029d1a0989689f5d3f4769e3c4a7
fh-pear <pear.cod4@gmail.com>
2017-05-06 23:52:13 -0400

    updated logging to use logpath from Config class removed consolehandler

--- src/NetProtocol.java
+++ src/NetProtocol.java
@@ -42,21 +42,14 @@ public final class NetProtocol
 			{
 				SimpleFormatter sf = new SimpleFormatter();
 
-				Handler filehandle = new FileHandler("fhrcon_debug.log", true);
+				Handler filehandle = new FileHandler(Config.getLogPath(), true);
 				filehandle.setFormatter(sf);
-				Handler consolehandle = new ConsoleHandler();
-				consolehandle.setFormatter(sf);
-
 				filehandle.setLevel(Config.getLoggingLevel());
-				consolehandle.setLevel(Config.getLoggingLevel());
 
 				logger.addHandler(filehandle);
-				logger.addHandler(consolehandle);
-
-
 				logger.setLevel(Config.getLoggingLevel());
 
-				logger.setUseParentHandlers(false);
+				logger.setUseParentHandlers(true);
 			} catch (IOException e)
 			{
 				logger.log(Level.WARNING, "Error setting up file stream for logging", e);
